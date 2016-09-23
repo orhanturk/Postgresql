8afa813b1292d0e56214747240383978366aa2a8
Orhan Türk <orhan.turk21@gmail.com>
2016-09-23 16:09:19 +0300

    Add files via upload

---
+++ siparis_coklukayit.sql
@@ -0,0 +1,12 @@
+﻿select * from tu_urun where urunkod='U21713'
+
+select * from t_irsaliye where tarih>=now()
+
+
+SELECT urunkod,
+COUNT(urunkod) AS toplam  
+FROM tsa_satinalmasiparisisub where sip_id=20492
+GROUP BY urunkod
+HAVING ( COUNT(urunkod) > 1 )
+
+select * from tsa_satinalmasiparisisub where sip_id=20492
